248a4fb2aec09159b43c6e309cd659bc7650e320
VicSoS <vubeda@utem.cl>
2023-06-24 00:18:41 -0400

    Cambios en funcion fetch y muestra de productos

--- src/Products.jsx
+++ src/Products.jsx
@@ -5,17 +5,44 @@ const section = '0';
 
 function Products() {
 
-  //const [articulos, setArticulos] = useState([])
-
   const URL = 'https://aquilabrand-api.onrender.com/products';
 
-  const showProducts = async () => {
+  const [products, setProducts] = useState();
+
+  const fetchApi = async () => {
     const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data);
-    //return data;
-  };
-  showProducts();
+    const responseJSON = await response.json();
+    setProducts(responseJSON);
+  }
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  return(
+  <div id="product-card">
+    <ul>
+      {!products ? 'Cargando...' : 
+        products.map((product, index) => {
+          return(<li key={product.PRODUCT_ID}>
+            <img
+              alt={product.PRODUCT_NAME}
+            />
+            <div id="titulo-producto">
+              <strong>{product.PRODUCT_NAME}</strong>
+            </div>
+            <div id="precio-producto">
+              <strong>${product.PRECIO}</strong>
+            </div>
+            <div id="boton-producto">
+              <button>Agregar al carrito</button>
+            </div>
+            </li>)
+        })
+      }
+    </ul>
+  </div>
+  );
 }
 
 export default Products;
