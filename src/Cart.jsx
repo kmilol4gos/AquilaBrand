bfaaff269d9f426b328910a4c3b9eb48f3708949
VicSoS <vubeda@utem.cl>
2023-06-25 21:32:05 -0400

    carrito de compras terminado teorico, uso de reducer. falta usarlo para mostrar y diseñar el product para que mande la info correcta.

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,5 +1,50 @@
 
+import { useCart } from "./hook/useCart";
+
+function CartProduct_Card({name, size, color, price, quantity, }){
+    return (
+        <div className="flex w-[90%] items-stretch pt-4 pb-6" id="card-product">
+            <div className="w-[30%] flex justify-center items-center mx-2">
+                <Link to="/product/idproducto">
+                    <img
+                        src={ImgProduct}
+                        alt=""
+                        className="w-full object-cover border-none"
+                    />
+                </Link>
+            </div>
+            <div className="flex justify-center flex-grow items-stretch ml-2">
+                <div className="w-full h-full flex flex-col justify-around text-white items-start">
+                    <Link to="/" className="font-bold text-xl no-underline">
+                        ${name}
+                    </Link>
+                    <div className="pb-7">
+                        <span className="font-light text-xs">Talla: ${size}</span>
+                        <span className="font-light text-xs">Color: ${color}</span>
+                    </div>
+                    <div className="pb-7">
+                        <span className="font-light text-xs">Cantidad: ${quantity}</span>
+                        <button onClick={addToCart}>+</button>
+                    </div>
+                    <div>
+                        <span className="font-normal text-xl before:content-['$']">
+                            ${price}
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col justify-center">
+                <button>
+                    <img src={IconDelete} alt="" />
+                </button>
+            </div>
+        </div>
+    )
+}
+
 export function Cart () {
+
+    const { cart, clearCart, addToCart } = useCart();
     return (
         <nav className="z-40 top-0 fixed right-0 h-screen flex flex-col w-[30rem]">
 					<div className="w-full top-20 h-full relative bg-black">
@@ -10,38 +55,15 @@ export function Cart () {
 							<div className="flex flex-col">
 								<div className="h-[85%]">
                                 <div className="my-2 flex flex-col justify-center bg-mainColor">
-                                    <div className="flex w-[90%] items-stretch pt-4 pb-6" id="card-product">
-                                        <div className="w-[30%] flex justify-center items-center mx-2">
-                                            <Link to="/product/idproducto">
-                                                <img
-                                                    src={ImgProduct}
-                                                    alt=""
-                                                    className="w-full object-cover border-none"
-                                                />
-                                            </Link>
-                                        </div>
-                                        <div className="flex justify-center flex-grow items-stretch ml-2">
-                                            <div className="w-full h-full flex flex-col justify-around text-white items-start">
-                                                <Link to="/" className="font-bold text-xl no-underline">
-                                                    Jeans Hombre
-                                                </Link>
-                                                <div className="pb-7">
-                                                    <span className="font-light text-xs">Talla: 45</span>
-                                                    <span className="font-light text-xs">Color: Negro</span>
-                                                </div>
-                                                <div>
-                                                    <span className="font-normal text-xl before:content-['$']">
-                                                        20.000
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="flex flex-col justify-center">
-                                            <button>
-                                                <img src={IconDelete} alt="" />
-                                            </button>
-                                        </div>
-                                    </div>
+                                    <ul>
+                                        {cart.map((product) => (
+                                            <CartProduct_Card
+                                                key={product.id}
+                                                addToCart = {() => addToCart(product)}
+                                                {...product}
+                                            />
+                                        ))}
+                                    </ul>
                                 </div>
 								</div>
 							</div>
--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -1,37 +1,35 @@
-import { createContext, useState } from "react";
+import { useReducer, createContext } from "react";
+import { cartReducer, cartInitialState } from "../reducers/cart";
 
 export const CartContext = createContext();
 
+function useCartReducer() {
+    const [state, dispatch] = useReducer(cartReducer, cartInitialState)
+
+    const addToCart = product => dispatch({
+        type: 'ADD_TO_CART',
+        payload: product
+    })
+
+    const removeFromCart = product => dispatch({
+        type: 'REMOVE_FROM_CART',
+        payload: product
+    })
+
+    const clearCart = () => dispatch({
+        type: 'CLEAR_CART'
+    })
+
+    return { state, addToCart, removeFromCart, clearCart }
+}
+
 export function CartProvider ({ children }) {
-    const [cart, setCart] = useState([]);
-
-    const addToCart = product => {
-        //check
-        const productInCartIndex = cart.findIndex(item => item.id === product.id);
-
-        if (productInCartIndex >= 0) {
-            const newCart = structuredClone(cart);
-            newCart[productInCartIndex].quantity += product.quantity;
-            return setCart(newCart);
-        }
-
-        //add si no esta en el carrito
-        setCart(prevState => ([
-            ...prevState,
-            {
-                ...product,
-                quantity: 1
-            }
-        ]))
-    };
-    const removeFromCart = product => {};
-    const clearCart = () => {
-        setCart([]);
-    };
+    
+    const { state, addToCart, removeFromCart, clearCart } = useCartReducer();
 
     return (
         <CartContext.Provider value={{ 
-            cart, 
+            cart: state, 
             addToCart, 
             removeFromCart, 
             clearCart }}
