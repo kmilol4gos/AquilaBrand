b7ecb61b4272d01afc259f72ef06c0f23bed7f01
VicSoS <vubeda@utem.cl>
2023-07-03 00:43:41 -0400

    Integracion endpoint /region y inicio idea imagenes

--- src/checkout.jsx
+++ src/checkout.jsx
@@ -15,435 +15,26 @@ import HomeIcon from "./assets/HomeIcon.svg";
 
 //aca se pide la informacion de la cliente.
 
-const RegionesYcomunas = {
-	regiones: [
-		{
-			NombreRegion: "Arica y Parinacota",
-			comunas: ["Arica", "Camarones", "Putre", "General Lagos"],
-		},
-		{
-			NombreRegion: "Tarapacá",
-			comunas: [
-				"Iquique",
-				"Alto Hospicio",
-				"Pozo Almonte",
-				"Camiña",
-				"Colchane",
-				"Huara",
-				"Pica",
-			],
-		},
-		{
-			NombreRegion: "Antofagasta",
-			comunas: [
-				"Antofagasta",
-				"Mejillones",
-				"Sierra Gorda",
-				"Taltal",
-				"Calama",
-				"Ollagüe",
-				"San Pedro de Atacama",
-				"Tocopilla",
-				"María Elena",
-			],
-		},
-		{
-			NombreRegion: "Atacama",
-			comunas: [
-				"Copiapó",
-				"Caldera",
-				"Tierra Amarilla",
-				"Chañaral",
-				"Diego de Almagro",
-				"Vallenar",
-				"Alto del Carmen",
-				"Freirina",
-				"Huasco",
-			],
-		},
-		{
-			NombreRegion: "Coquimbo",
-			comunas: [
-				"La Serena",
-				"Coquimbo",
-				"Andacollo",
-				"La Higuera",
-				"Paiguano",
-				"Vicuña",
-				"Illapel",
-				"Canela",
-				"Los Vilos",
-				"Salamanca",
-				"Ovalle",
-				"Combarbalá",
-				"Monte Patria",
-				"Punitaqui",
-				"Río Hurtado",
-			],
-		},
-		{
-			NombreRegion: "Valparaíso",
-			comunas: [
-				"Valparaíso",
-				"Casablanca",
-				"Concón",
-				"Juan Fernández",
-				"Puchuncaví",
-				"Quintero",
-				"Viña del Mar",
-				"Isla de Pascua",
-				"Los Andes",
-				"Calle Larga",
-				"Rinconada",
-				"San Esteban",
-				"La Ligua",
-				"Cabildo",
-				"Papudo",
-				"Petorca",
-				"Zapallar",
-				"Quillota",
-				"Calera",
-				"Hijuelas",
-				"La Cruz",
-				"Nogales",
-				"San Antonio",
-				"Algarrobo",
-				"Cartagena",
-				"El Quisco",
-				"El Tabo",
-				"Santo Domingo",
-				"San Felipe",
-				"Catemu",
-				"Llaillay",
-				"Panquehue",
-				"Putaendo",
-				"Santa María",
-				"Quilpué",
-				"Limache",
-				"Olmué",
-				"Villa Alemana",
-			],
-		},
-		{
-			NombreRegion: "Región del Libertador Gral. Bernardo O’Higgins",
-			comunas: [
-				"Rancagua",
-				"Codegua",
-				"Coinco",
-				"Coltauco",
-				"Doñihue",
-				"Graneros",
-				"Las Cabras",
-				"Machalí",
-				"Malloa",
-				"Mostazal",
-				"Olivar",
-				"Peumo",
-				"Pichidegua",
-				"Quinta de Tilcoco",
-				"Rengo",
-				"Requínoa",
-				"San Vicente",
-				"Pichilemu",
-				"La Estrella",
-				"Litueche",
-				"Marchihue",
-				"Navidad",
-				"Paredones",
-				"San Fernando",
-				"Chépica",
-				"Chimbarongo",
-				"Lolol",
-				"Nancagua",
-				"Palmilla",
-				"Peralillo",
-				"Placilla",
-				"Pumanque",
-				"Santa Cruz",
-			],
-		},
-		{
-			NombreRegion: "Región del Maule",
-			comunas: [
-				"Talca",
-				"ConsVtución",
-				"Curepto",
-				"Empedrado",
-				"Maule",
-				"Pelarco",
-				"Pencahue",
-				"Río Claro",
-				"San Clemente",
-				"San Rafael",
-				"Cauquenes",
-				"Chanco",
-				"Pelluhue",
-				"Curicó",
-				"Hualañé",
-				"Licantén",
-				"Molina",
-				"Rauco",
-				"Romeral",
-				"Sagrada Familia",
-				"Teno",
-				"Vichuquén",
-				"Linares",
-				"Colbún",
-				"Longaví",
-				"Parral",
-				"ReVro",
-				"San Javier",
-				"Villa Alegre",
-				"Yerbas Buenas",
-			],
-		},
-		{
-			NombreRegion: "Region del Ñuble",
-			comunas: [
-				"Cobquecura",
-				"Coelemu",
-				"Ninhue",
-				"Portezuelo",
-				"Quirihue",
-				"Ránquil",
-				"Trehauco",
-				"Bulnes",
-				"Chillán Viejo",
-				"Chillán",
-				"El Carmen",
-				"Pemuco",
-				"Pinto",
-				"Quillón",
-				"San Ignacio",
-				"Yungay",
-				"Coihueco",
-				"Ñiquén",
-				"San Carlos",
-				"San Fabian",
-				"San Nicolás",
-			],
-		},
-		{
-			NombreRegion: "Región del Biobío",
-			comunas: [
-				"Concepción",
-				"Coronel",
-				"Chiguayante",
-				"Florida",
-				"Hualqui",
-				"Lota",
-				"Penco",
-				"San Pedro de la Paz",
-				"Santa Juana",
-				"Talcahuano",
-				"Tomé",
-				"Hualpén",
-				"Lebu",
-				"Arauco",
-				"Cañete",
-				"Contulmo",
-				"Curanilahue",
-				"Los Álamos",
-				"Tirúa",
-				"Los Ángeles",
-				"Antuco",
-				"Cabrero",
-				"Laja",
-				"Mulchén",
-				"Nacimiento",
-				"Negrete",
-				"Quilaco",
-				"Quilleco",
-				"San Rosendo",
-				"Santa Bárbara",
-				"Tucapel",
-				"Yumbel",
-				"Alto Biobío",
-			],
-		},
-		{
-			NombreRegion: "Región de la Araucanía",
-			comunas: [
-				"Temuco",
-				"Carahue",
-				"Cunco",
-				"Curarrehue",
-				"Freire",
-				"Galvarino",
-				"Gorbea",
-				"Lautaro",
-				"Loncoche",
-				"Melipeuco",
-				"Nueva Imperial",
-				"Padre las Casas",
-				"Perquenco",
-				"Pitrufquén",
-				"Pucón",
-				"Saavedra",
-				"Teodoro Schmidt",
-				"Toltén",
-				"Vilcún",
-				"Villarrica",
-				"Cholchol",
-				"Angol",
-				"Collipulli",
-				"Curacautín",
-				"Ercilla",
-				"Lonquimay",
-				"Los Sauces",
-				"Lumaco",
-				"Purén",
-				"Renaico",
-				"Traiguén",
-				"Victoria",
-			],
-		},
-		{
-			NombreRegion: "Región de Los Ríos",
-			comunas: [
-				"Valdivia",
-				"Corral",
-				"Lanco",
-				"Los Lagos",
-				"Máfil",
-				"Mariquina",
-				"Paillaco",
-				"Panguipulli",
-				"La Unión",
-				"Futrono",
-				"Lago Ranco",
-				"Río Bueno",
-			],
-		},
-		{
-			NombreRegion: "Región de Los Lagos",
-			comunas: [
-				"Puerto Montt",
-				"Calbuco",
-				"Cochamó",
-				"Fresia",
-				"FruVllar",
-				"Los Muermos",
-				"Llanquihue",
-				"Maullín",
-				"Puerto Varas",
-				"Castro",
-				"Ancud",
-				"Chonchi",
-				"Curaco de Vélez",
-				"Dalcahue",
-				"Puqueldón",
-				"Queilén",
-				"Quellón",
-				"Quemchi",
-				"Quinchao",
-				"Osorno",
-				"Puerto Octay",
-				"Purranque",
-				"Puyehue",
-				"Río Negro",
-				"San Juan de la Costa",
-				"San Pablo",
-				"Chaitén",
-				"Futaleufú",
-				"Hualaihué",
-				"Palena",
-			],
-		},
-		{
-			NombreRegion: "Región Asén del Gral. Carlos Ibáñez del Campo",
-			comunas: [
-				"Coihaique",
-				"Lago Verde",
-				"Aisén",
-				"Cisnes",
-				"Guaitecas",
-				"Cochrane",
-				"O’Higgins",
-				"Tortel",
-				"Chile Chico",
-				"Río Ibáñez",
-			],
-		},
-		{
-			NombreRegion: "Región de Magallanes y de la Antárica Chilena",
-			comunas: [
-				"Punta Arenas",
-				"Laguna Blanca",
-				"Río Verde",
-				"San Gregorio",
-				"Cabo de Hornos (Ex Navarino)",
-				"AntárVca",
-				"Porvenir",
-				"Primavera",
-				"Timaukel",
-				"Natales",
-				"Torres del Paine",
-			],
-		},
-		{
-			NombreRegion: "Región Metropolitana de Santiago",
-			comunas: [
-				"Cerrillos",
-				"Cerro Navia",
-				"Conchalí",
-				"El Bosque",
-				"Estación Central",
-				"Huechuraba",
-				"Independencia",
-				"La Cisterna",
-				"La Florida",
-				"La Granja",
-				"La Pintana",
-				"La Reina",
-				"Las Condes",
-				"Lo Barnechea",
-				"Lo Espejo",
-				"Lo Prado",
-				"Macul",
-				"Maipú",
-				"Ñuñoa",
-				"Pedro Aguirre Cerda",
-				"Peñalolén",
-				"Providencia",
-				"Pudahuel",
-				"Quilicura",
-				"Quinta Normal",
-				"Recoleta",
-				"Renca",
-				"San Joaquín",
-				"San Miguel",
-				"San Ramón",
-				"Vitacura",
-				"Puente Alto",
-				"Pirque",
-				"San José de Maipo",
-				"Colina",
-				"Lampa",
-				"TilVl",
-				"San Bernardo",
-				"Buin",
-				"Calera de Tango",
-				"Paine",
-				"Melipilla",
-				"Alhué",
-				"Curacaví",
-				"María Pinto",
-				"San Pedro",
-				"Talagante",
-				"El Monte",
-				"Isla de Maipo",
-				"Padre Hurtado",
-				"Peñaflor",
-			],
-		},
-		// Resto de regiones...
-	],
-};
-
 const App = () => {
 	const [selectedRegion, setSelectedRegion] = useState("");
 	const [selectedComuna, setSelectedComuna] = useState("");
+
+	const [comunas, setComunas] = useState([]);
+	const [regiones, setRegiones] = useState([]);
+
+	const fetchApi = async () => {
+		const response = await fetch(
+			"http://localhost:3000/region"
+		);
+		const data = await response.json();
+		setRegiones(data[1]);
+		setComunas(data[0]);
+	};
+
+	useEffect(() => {
+		fetchApi();
+	}, []);
+
 
 	const handleRegionChange = (e) => {
 		const selectedRegion = e.target.value;
@@ -456,6 +47,13 @@ const App = () => {
 		setSelectedComuna(selectedComuna);
 	};
 
+	let newState = [];
+
+	if(!regiones){
+		return <h1>Cargando...</h1>
+	}
+	newState = comunas.filter((item) => item.REGION_NOMBRE === selectedRegion)
+
 	return (
 		<div className="flex flex-col gap-4">
 			<select
@@ -464,9 +62,9 @@ const App = () => {
 				onChange={handleRegionChange}
 			>
 				<option value="">Seleccione región</option>
-				{RegionesYcomunas.regiones.map((region) => (
-					<option key={region.NombreRegion} value={region.NombreRegion}>
-						{region.NombreRegion}
+				{regiones.map((region) => (
+					<option key={region.REGION_NOMBRE} value={region.REGION_NOMBRE}>
+						{region.REGION_NOMBRE}
 					</option>
 				))}
 			</select>
@@ -476,14 +74,12 @@ const App = () => {
 				onChange={handleComunaChange}
 			>
 				<option value="">Seleccione comuna</option>
-				{selectedRegion &&
-					RegionesYcomunas.regiones
-						.find((region) => region.NombreRegion === selectedRegion)
-						.comunas.map((comuna) => (
-							<option key={comuna} value={comuna}>
-								{comuna}
-							</option>
-						))}
+				{
+					newState.map((comuna) => (
+						<option key={comuna.COMUNA_NOMBRE} value={comuna.COMUNA_NOMBRE}>
+							{comuna.COMUNA_NOMBRE}
+						</option>
+					))}
 			</select>
 			{/* <button
 				onClick={() => {
@@ -537,7 +133,7 @@ function Product_Card({
 }) {
 	return (
 		<div
-			key={PRODUCT_ID}
+			key={PRODUCT_NAME}
 			className="bg-mainColor flex justify-between p-5 text-white"
 		>
 			<div className="bg-white flex items-center w-[30%]">
