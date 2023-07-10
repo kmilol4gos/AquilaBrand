8ce5c39c6c3b2181671ba98f0410fba9153210c0
VicSoS <vubeda@utem.cl>
2023-07-09 22:11:54 -0400

    a

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,5 +1,5 @@
 import Webpay from "./assets/webpay.svg";
-import Aquila from "./assets/Aguila_Sola.png";
+import Aquila from "./assets/Aguila-blanca.png";
 import { Link } from "react-router-dom";
 
 function Footer() {
@@ -9,7 +9,7 @@ function Footer() {
 				<div className=" mx-auto my-auto flex flex-col justify-center items-center">
 					<div id="metodopago" className="flex flex-col justify-center items-center">
 						<h3 className="text-sm font-normal my-4">Compra segura con</h3>
-						<img src={Webpay} alt="Logo de WebPay" className="relative w-[140%]"/>
+						<img src={Webpay} alt="Logo de WebPay" className="relative w-[100%]"/>
 					</div>
 					<div id="info" className="flex flex-col justify-center items-center absolute bottom-0 mb-auto space-y-3">
 						<img src={Aquila} alt="Logo Empresa" className="relative w-[25%]" />
