18ae81c1cf338200877f6c50944c2765005d7ed5
Kmilo0816 <k4milolagos@gmail.com>
2023-07-09 22:33:18 -0400

    Se cambia footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,26 +1,35 @@
 import Webpay from "./assets/webpay.svg";
 import Aquila from "./assets/Aguila-blanca.png";
-import { Link } from "react-router-dom";
 
 function Footer() {
 	return (
-		<footer className="bottom-0 right-0 h-60 bg-black w-screen relative inline-block">
-			<section className="mx-40 display-flex h-full items-stretch text-white">
-				<div className=" mx-auto my-auto flex flex-col justify-center items-center">
-					<div id="metodopago" className="flex flex-col justify-center items-center">
-						<h3 className="text-sm font-normal my-4">Compra segura con</h3>
-						<img src={Webpay} alt="Logo de WebPay" className="relative w-[100%]"/>
-					</div>
-					<div id="info" className="flex flex-col justify-center items-center absolute bottom-0 mb-auto space-y-3">
-						<img src={Aquila} alt="Logo Empresa" className="relative w-[25%]" />
-						<div className="flex flex-col justify-center items-center">
-						<p className="text-sm">© AquilaBrand 2023. Todos los derechos reservados</p>
-						<p className="text-sm">Sitio seguro con criptografia (SSL)</p>
+		<footer className="bottom-0 right-0 h-full md:h-60 bg-black w-screen relative inline-block">
+			<section className="md:mx-40 h-full text-white py-4">
+				<div className=" flex flex-col md:flex-row justify-around h-full w-full">
+					<div
+						id="metodopago"
+						className="flex flex-col justify-center items-center"
+					>
+						<h3 className="text-xl font-normal my-4">Compra segura con</h3>
+						<div>
+							<img src={Webpay} alt="Logo de WebPay" className="w-full" />
 						</div>
 					</div>
-					
+					<div
+						id="info"
+						className="flex flex-col items-center justify-center gap-5"
+					>
+						<div className="md:w-56 w-48">
+							<img src={Aquila} alt="Logo Empresa" className="w-full h-full" />
+						</div>
+						<div className="flex flex-col justify-center items-center">
+							<p className="text-sm">
+								© AquilaBrand 2023. Todos los derechos reservados
+							</p>
+							<p className="text-sm">Sitio seguro con criptografia (SSL)</p>
+						</div>
+					</div>
 				</div>
-				
 			</section>
 		</footer>
 	);
